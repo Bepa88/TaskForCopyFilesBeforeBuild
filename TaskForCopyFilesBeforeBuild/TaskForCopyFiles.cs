ea3a1eb85c5ffcdc50a72c8c3d16c6c5c6c3d263
BIKTOP <biktop512@gmail.com>
2023-06-19 16:57:44 +0300

    Add project

---
+++ TaskForCopyFilesBeforeBuild/TaskForCopyFiles.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace TaskForCopyFilesBeforeBuild
+{
+    public class TaskForCopyFiles : Task
+    {
+        private string sourceDir;// = "C:\\From";
+        private string targetDir;// = "C:\\To";
+
+        //[Required]
+        public string SourceDir
+        {
+            get
+            {
+                return sourceDir;
+            }
+            set
+            {
+                sourceDir = value;
+            }
+        }
+
+        //[Required]
+        public string TargetDir
+        {
+            get
+            {
+                return targetDir;
+            }
+            set
+            {
+                targetDir = value;
+            }
+        }
+        public override bool Execute()
+        {
+
+            Copy(sourceDir, targetDir);
+            return true;
+        }
+
+        void Copy(string source, string target)
+        {
+            Directory.CreateDirectory(target);
+
+            foreach (var file in Directory.GetFiles(source))
+                File.Copy(file, Path.Combine(target, Path.GetFileName(file)));
+
+            foreach (var directory in Directory.GetDirectories(source))
+                Copy(directory, Path.Combine(target, Path.GetFileName(directory)));
+        }
+
+}
+}
